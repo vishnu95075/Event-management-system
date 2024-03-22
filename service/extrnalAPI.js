42f38e25df31240c5b92c31d6c06f0371dcff95d
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-03-22 21:57:47 +0530

    assignment completed

--- service/extrnalAPI.js
+++ service/extrnalAPI.js
@@ -2,14 +2,9 @@ const axios = require('axios');
 
 exports.distanceCalculate = async (userLatitude, userLongitude, latitude, longitude) => {
   const distanceAPI = 'https://gg-backend-assignment.azurewebsites.net/api/Distance';
-  try {
     const url = `${distanceAPI}?code=${process.env.DISTANCE_API_CODE}==&latitude1=${userLatitude}&longitude1=${userLongitude}&latitude2=${latitude}&longitude2=${longitude}`;
     const response = await axios.get(url);
     return response.data.distance;
-  } catch (error) {
-    console.error('Error calculating distance:', error.response.data);
-    throw new Error('Error calculating distance');
-  }
 
 };
 
@@ -20,12 +15,8 @@ exports.weatherCondition = async (city, date) => {
   const new_date =  `${year}-${month}-${day}`;
 
   const weatherAPI = 'https://gg-backend-assignment.azurewebsites.net/api/Weather';
-  try {
     const url = `${weatherAPI}?code=${process.env.WEATHER_API_CODE}==&city=${encodeURIComponent(city)}&date=${new_date}`;
     const response = await axios.get(url);
     return response.data.weather;
-  } catch (error) {
-    console.error('Error fetching weather:', error.response.data);
-    throw new Error('Error fetching weather');
-  }
+  
 }
